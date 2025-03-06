c69a2e0d90367258ed0c83768bf6d6c7154e82ba
APoojitha763 <poojithaadusumilli123@gmail.com>
2025-03-06 00:29:28 -0600

    added in dto

--- applicationbank/src/main/java/com/example/applicationbank/dto/AccountDto.java
+++ applicationbank/src/main/java/com/example/applicationbank/dto/AccountDto.java
@@ -11,4 +11,7 @@ public class AccountDto {
     private String accountNumber;
     private String branchName;
     private String ifscCode;
+    private String bankName;
+    private String accountStatus;
+    private String accountCreationDate;
 }
