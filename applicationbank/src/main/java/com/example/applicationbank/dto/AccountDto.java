fb0599649db5972ba13010a91f07e12ecfda25cc
APoojitha763 <poojithaadusumilli123@gmail.com>
2025-03-05 22:52:45 -0600

    Initial commit creating project on bank application

---
+++ applicationbank/src/main/java/com/example/applicationbank/dto/AccountDto.java
@@ -0,0 +1,11 @@
+package com.example.applicationbank.dto;
+
+import lombok.Data;
+
+@Data
+public class AccountDto {
+    private Long id;
+    private String accountHolderName;
+    private double balane;
+
+}
---
+++ applicationbank/src/main/java/com/example/applicationbank/service/AccountService.java
@@ -0,0 +1,9 @@
+package com.example.applicationbank.service;
+
+import com.example.applicationbank.entity.Account;
+
+public interface AccountService{
+    // Define methods for the service here
+    Account created(Account account);
+
+}
